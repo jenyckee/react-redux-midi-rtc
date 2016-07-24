a4babf08d6f4924dd6947f93ca69bde69d11e400
Jeroen Nyckees <jenyckee@jenyckee.local>
2016-07-24 18:44:12 +0200

    notedown and noteup

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,25 +1,29 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { noteDown, noteUp } from '../../modules/midi'
+import { emitRTC } from '../../modules/conductor'
 import classes from './Note.scss'
 import cx from 'classnames'
 
 export class Note extends React.Component<void, Props, void> {
   classList () {
     return cx({
-      note: true,
-      played: this.props.midiState.get(this.props.note.midi())
+      [classes.note]: true,
+      [classes.note.played]: this.props.midi.get(this.props.note.midi())
     })
   }
   noteDown () {
-    this.props.noteDown({ midi: this.props.note.midi() })
+    this.props.emitRTC([0x90, 60, 0x7f])
+    // this.props.noteDown({ midi: this.props.note.midi() })
   }
   noteUp () {
-    this.props.noteUp({ midi: this.props.note.midi() })
+    this.props.emitRTC([0x80, 60, 0x7f])
+
+    // this.props.noteUp({ midi: this.props.note.midi() })
   }
   render () {
     return (
-      <div onMouseDown={this.noteDown.bind(this)} onMouseUp={this.noteUp.bind(this)}></div>
+      <div className={this.classList()} onMouseDown={this.noteDown.bind(this)} onMouseUp={this.noteUp.bind(this)}></div>
     )
   }
 }
@@ -29,5 +33,6 @@ const mapStateToProps = (state) => ({
 })
 export default connect(mapStateToProps, {
   noteDown: noteDown,
-  noteUp: noteUp
+  noteUp: noteUp,
+  emitRTC: emitRTC,
 })(Note)
